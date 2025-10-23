c6199f0fc19047021cb5e20a460f3e13b5ecd9a0
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-23 13:39:22 +0200

    dev: added commandlinerunner for testing of product entity and repository

--- src/main/java/dev/jeroen/plushie_backend/PlushieBackendApplication.java
+++ src/main/java/dev/jeroen/plushie_backend/PlushieBackendApplication.java
@@ -1,7 +1,14 @@
 package dev.jeroen.plushie_backend;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.jeroen.plushie_backend.entities.Product;
+import dev.jeroen.plushie_backend.repositories.ProductRepository;
 
 @SpringBootApplication
 public class PlushieBackendApplication {
@@ -9,5 +16,20 @@ public class PlushieBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlushieBackendApplication.class, args);
 	}
+
+	@Bean
+  	CommandLineRunner runner(ProductRepository repository) {
+    	return args -> {
+
+			Product product = new Product();
+			product.setName("Red Shrimp Large");
+			product.setDescription("Large plushie of a red shrimp.");
+
+			repository.save(product);
+			Product saved = repository.findById(product.getId()).orElseThrow(NoSuchElementException::new);
+			System.out.println(saved.toString());
+
+    	};
+  	}
 
 }
