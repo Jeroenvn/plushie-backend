b4de9ea1be39945c695f997f234a258f990f55f2
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-23 11:33:06 +0200

    Initial commit

---
+++ src/main/java/dev/jeroen/plushie_backend/PlushieBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.jeroen.plushie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlushieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlushieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jeroen/plushie_backend/PlushieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeroen.plushie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlushieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
