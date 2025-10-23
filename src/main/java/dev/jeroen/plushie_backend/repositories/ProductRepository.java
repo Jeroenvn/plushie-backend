5243b2896978cf18627fa812c9d939ba9b22d1f1
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-23 13:38:01 +0200

    feat: created minimal product repository

---
+++ src/main/java/dev/jeroen/plushie_backend/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package dev.jeroen.plushie_backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+import dev.jeroen.plushie_backend.entities.Product;
+
+public interface ProductRepository extends Repository<Product, Long>{
+
+    Product save(Product product);
+
+    Optional<Product> findById(Long id);
+}
