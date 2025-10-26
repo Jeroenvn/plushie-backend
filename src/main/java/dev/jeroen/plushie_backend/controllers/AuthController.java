f65eb675f8c73fae98bf7a881d54881f5a17867a
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-27 00:51:59 +0100

    chore: removed unused import

--- src/main/java/dev/jeroen/plushie_backend/controllers/AuthController.java
+++ src/main/java/dev/jeroen/plushie_backend/controllers/AuthController.java
@@ -1,7 +1,5 @@
 package dev.jeroen.plushie_backend.controllers;
 
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
