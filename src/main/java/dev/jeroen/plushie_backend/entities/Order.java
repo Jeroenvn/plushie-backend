b443b35fe7be1a2ddf62a95f58fd3b4cfe82df9c
Jeroen van Noort <s1137642@student.hsleiden.nl>
2025-10-27 04:21:16 +0100

    fix: forgot @Entity for new classes

--- src/main/java/dev/jeroen/plushie_backend/entities/Order.java
+++ src/main/java/dev/jeroen/plushie_backend/entities/Order.java
@@ -1,5 +1,6 @@
 package dev.jeroen.plushie_backend.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -7,6 +8,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
+@Entity
 public class Order {
 
     @Id
